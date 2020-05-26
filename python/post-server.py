e0f8952563d0c69c024305168a24d6db585ae670
Liam Fruzyna <liamfruzyna@gmail.com>
2020-05-26 17:41:31 -0500

    Organized repo, cleaned up server

--- python/post-server.py
+++ python/post-server.py
@@ -7,6 +7,8 @@ UPLOAD_PATH = 'uploads/'
 VALID_PATHS = ['/config', '/scripts', '/styles', '/uploads', '/favicon.ico', '/index.html', '/scout.html', '/selection.html']
 
 class ServerHandler(http.server.SimpleHTTPRequestHandler):
+
+    # default response
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -16,51 +18,69 @@ class ServerHandler(http.server.SimpleHTTPRequestHandler):
         logging.error(self.headers)
         print(self.path)
         files = []
+
+        # request for results
         if self.path == '/getPitResultNames':
             files = [f for f in listdir(UPLOAD_PATH) if isfile(join(UPLOAD_PATH, f)) and f.startswith('pit-') and f.endswith('.json')]
         elif self.path == '/getMatchResultNames':
             files = [f for f in listdir(UPLOAD_PATH) if isfile(join(UPLOAD_PATH, f)) and f.startswith('match-') and f.endswith('.json')]
         elif self.path == '/getNoteNames':
             files = [f for f in listdir(UPLOAD_PATH) if isfile(join(UPLOAD_PATH, f)) and f.startswith('notes-') and f.endswith('.json')]
+        
+        # about page, used to check server version
         elif self.path == '/about':
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(str.encode('<!DOCTYPE html><html lang="en"><html><head><meta charset="utf-8"/><title>LiamRank</title></head><body><h1>Liam Rank</h1>post-server.py Python3 post-server<br>2020 Liam Fruzyna<br><a href="https://github.com/mail929/LiamRank">MPL Licensed on GitHub</a></body></html>'))
             return
+
         else:
+            # check if its a known file
             for path in VALID_PATHS:
                 if self.path.startswith(path) or self.path == '/':
+                    # add /assets in front of files
+                    if '.' in path or self.path == '/':
+                        self.path = '/assets{0}'.format(self.path)
                     return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            
+            # send 404 otherwise
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(str.encode('<!DOCTYPE html><html lang="en"><html><head><meta charset="utf-8"/><title>LiamRank</title></head><body><h1>Liam Rank - 404</h1>{0} not found!</body></html>'.format(self.path)))
             return
+
+        # send joined results
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(str.encode(','.join(files)))
 
+    # allow requests without credentials
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
         http.server.SimpleHTTPRequestHandler.end_headers(self)
 
+    # receive results uploads
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         logging.info('POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n',
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
+        # make response
         self._set_response()
         self.wfile.write('POST request for {}'.format(self.path).encode('utf-8'))
+
+        # save result
         upload = post_data.decode('utf-8').split('|||')
-        with open(UPLOAD_PATH + upload[0] + '.json', 'w') as f:
-            f.write(upload[1])
+        if len(upload) > 1:
+            with open(UPLOAD_PATH + upload[0] + '.json', 'w') as f:
+                f.write(upload[1])
 
+# start HTTP server
 Handler = ServerHandler
-
 httpd = socketserver.TCPServer(('', PORT), Handler)
-
 print('Serving at port: ', PORT)
 httpd.serve_forever()
