58000097fd146e631bc40470ad098056b34e12b8
Liam Fruzyna <liamfruzyna@gmail.com>
2022-01-21 18:43:03 -0600

    Fixed pivot csv export and added header import

--- scripts/pivot.js
+++ scripts/pivot.js
@@ -29,7 +29,8 @@ function init_page(contents_card, buttons_container)
     contents_card.innerHTML = '<table id="results_tab"></table>'
     buttons_container.innerHTML = build_column_frame('', [build_select('type_form', '', SORT_OPTIONS, 'Mean', 'build_table()')]) +
                                     build_column_frame('', [build_link_button('save_list', 'Save to Pick List', 'save_pick_list()')]) +
-                                    build_column_frame('', [build_button('export_table', 'Export Table', 'export_table()')])
+                                    build_column_frame('', [build_button('export_table', 'Export Table', 'export_table()')]) +
+                                    build_column_frame('', [build_button('upload_csv', 'Import Headers', 'upload_csv()')])
         
     meta = get_result_meta(type, year)
 
@@ -367,6 +368,73 @@ function build_table(sort_by='', reverse=false)
 }
 
 /**
+ * function:    upload_csv
+ * paramters:   none
+ * returns:     none
+ * description: Creates a file prompt to upload a CSV file.
+ */
+function upload_csv()
+{
+    var input = document.createElement('input')
+    input.type = 'file'
+    input.accept = 'text/csv'
+    input.onchange = import_headers
+    input.click()
+}
+
+/**
+ * function:    import_headers
+ * paramters:   response containing CSV file
+ * returns:     none
+ * description: Loads in headers from a CSV file.
+ */
+function import_headers(event)
+{
+    let file = event.target.files[0]
+    let reader = new FileReader()
+    reader.readAsText(file, 'UTF-8')
+    reader.onload = readerEvent => {
+        let lines = readerEvent.target.result.split('\n')
+        let headers = lines[0].split(',').map(l => l.trim())
+        
+        let keys = Array.prototype.filter.call(document.getElementsByClassName('pit_option'), item => item.id.startsWith('o')).map(item => item.id.replace('option_', ''))
+        for(let key of keys)
+        {
+            let type = meta[key].type
+            if (type == 'checkbox' || type == 'select' || type == 'dropdown')
+            {
+                for (let op of meta[key].options)
+                {
+                    // handle boolean labels
+                    if (op === 'false')
+                    {
+                        op = 'Not'
+                    }
+                    else if (op === 'true')
+                    {
+                        op = ''
+                    }
+                    if (headers.includes(`${meta[key].name} ${op}`))
+                    {
+                        document.getElementById(`option_${key}`).classList.add('selected')
+                        break
+                    }
+                }
+            }
+            else
+            {
+                if (headers.includes(meta[key].name))
+                {
+                    document.getElementById(`option_${key}`).classList.add('selected')
+                }
+            }
+        }
+        
+        build_table()
+    }
+}
+
+/**
  * function:    export_table
  * parameters:  none
  * returns:     none
@@ -374,37 +442,89 @@ function build_table(sort_by='', reverse=false)
  */
 function export_table()
 {
-    let keys = get_selected_keys()
+    let selected = get_selected_keys()
+    let method = get_selected_option('type_form')
 
-    // build csv
-    let line = 'Totals'
-    for (let key of keys)
+    let filter_teams = get_secondary_selected_keys()
+    teams = all_teams.filter(team => filter_teams.includes(team.toString()))
+
+    // header row
+    let header = ['Team']
+    for (let key of selected)
     {
-        let val = avg_results(results, key, meta[key].type, get_selected_option('type_form'))
-        line += ',' + get_value(meta, key, val)
+        let name = ''
+        if (key.includes('-'))
+        {
+            let parts = key.split('-')
+            let label = parts[1]
+            // handle boolean labels
+            if (label === 'false')
+            {
+                label = 'Not'
+            }
+            else if (label === 'true')
+            {
+                label = ''
+            }
+            name = `${meta[parts[0]].name} ${label}`
+        }
+        else
+        {
+            name = meta[key].name
+        }
+        header.push(name)
     }
-    
-    let lines = [ 'Team,' + keys.map(key => meta[key].name).join(','), line]
+
+    // data rows
+    let rows = [header.join(',')]
     for (let team of teams)
     {
+        let row = [team]
         let team_results = get_team_results(results, team)
         if (Object.keys(team_results).length > 0)
         {
-            line = team
-            for (let key of keys)
+            for (let key of selected)
             {
-                let val = avg_results(team_results, key, meta[key].type, get_selected_option('type_form'))
+                let label = ''
+                if (key.includes('-'))
+                {
+                    let parts = key.split('-')
+                    key = parts[0]
+                    label = parts[1]
+                }
+                let type = meta[key].type
+
+                let val = avg_results(team_results, key, type, method)
                 let valStr = get_value(meta, key, val)
-                line += ',' + valStr
+                let base = avg_results(results, key, type, method)
+                // build a value string of percents for discrete inputs
+                if (type == 'checkbox' || type == 'select' || type == 'dropdown')
+                {
+                    let res = avg_results(team_results, key, type, method, meta[key].options)
+                    val = (100 * res[label] / Object.values(res).reduce((a, b) => a + b, 0))
+                    valStr = val.toFixed(1) + '%'
+
+                    res = avg_results(results, key, type, method, meta[key].options)
+                    base = (100 * res[label] / Object.values(res).reduce((a, b) => a + b, 0))
+                }
+
+                if (typeof base === 'number' && !key.startsWith('meta'))
+                {            
+                    // add std dev if proper number
+                    if (method == 0 && type != 'select' && type != 'dropdown' && type != 'checkbox')
+                    {
+                        valStr += ` (${get_value(meta, key, avg_results(team_results, key, type, 5))})`
+                    }
+                }
+                row.push(valStr)
             }
-            lines.push(line)
+            rows.push(row.join(','))
         }
     }
-    let csv = lines.join('\n').replace(/,NaN/g, ',')
 
     // download CSV
     let element = document.createElement('a')
-    element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv))
+    element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(rows.join('\n')))
     element.setAttribute('download', `${event_id}-pivot-export.csv`)
 
     element.style.display = 'none'
