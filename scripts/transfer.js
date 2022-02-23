b6d4d11f93c8a382ce8bab68c58cc86f28eba910
Liam Fruzyna <liamfruzyna@gmail.com>
2022-02-23 17:26:05 -0600

    Added cookies to reset

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -174,7 +174,20 @@ function reset()
 {
     if (confirm('Delete all configuration, results, and other app data?'))
     {
+        // clear storage
         localStorage.clear()
+        
+        // clear cookies
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++)
+        {
+            let cookie = cookies[i];
+            let eqPos = cookie.indexOf('=');
+            let name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+            document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+        }
+
+        // clear offline pages
         caches.keys().then(keyList => {
             let ret = Promise.all(keyList.map(key => {
                 return caches.delete(key)
