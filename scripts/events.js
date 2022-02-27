f245a45a4c7185b1294486a49f0a93e7e59d15ad
Liam Fruzyna <liamfruzyna@gmail.com>
2022-02-26 19:32:23 -0600

    Highlight winners and finalists in other events page (#138)

--- scripts/events.js
+++ scripts/events.js
@@ -36,28 +36,29 @@ function init_page()
                     return response.json()
                 })
                 .then(data => {
-                    // get teams' event codes and add team to a map
-                    let team_events = data.map(e => e.event_code)
-                    for (let e of team_events)
+                    for (let d of data)
                     {
+                        let e = d.event_code
                         if (!Object.keys(events).includes(e))
                         {
-                            events[e] = { teams: [team.team_number] }
+                            events[e] = { teams: {} }
+                            events[e].name = d.name
+                            events[e].start = d.start_date
                         }
-                        else
-                        {
-                            events[e].teams.push(team.team_number)
+                        events[e].teams[team.team_number] = {
+                            award: '',
+                            label: ''
                         }
                     }
 
                     // if all team events have been collected get event data
-                    if (++team_count == teams.length)
+                    if (++team_count == Object.keys(teams).length)
                     {
                         let keys = Object.keys(events)
                         let received = 0
                         for (let e of keys)
                         {
-                            fetch(`https://www.thebluealliance.com/api/v3/event/${year}${e}/simple${build_query({[TBA_KEY]: API_KEY})}`)
+                            fetch(`https://www.thebluealliance.com/api/v3/event/${year}${e}/awards${build_query({[TBA_KEY]: API_KEY})}`)
                                 .then(response => {
                                     if (response.status == 401)
                                     {
@@ -66,31 +67,67 @@ function init_page()
                                     return response.json()
                                 })
                                 .then(data => {
-                                    // data event metadata to object
-                                    events[e].name = data.name
-                                    events[e].start = data.start_date
+                                    for (let award of data)
+                                    {
+                                        switch (award.award_type)
+                                        {
+                                            case 1:
+                                            case 2:
+                                                // winner
+                                                for (let i in award.recipient_list)
+                                                {
+                                                    let team = award.recipient_list[i].team_key.substr(3)
+                                                    if (Object.keys(events[e].teams).includes(team))
+                                                    {
+                                                        if (award.award_type == 1)
+                                                        {
+                                                            events[e].teams[team].award = 'background-color: #0f4bcb; color: #ffffff'
+                                                        }
+                                                        else
+                                                        {
+                                                            events[e].teams[team].award = 'background-color: #c0c0c0; color: #000000'
+                                                        }
+                                                        switch (parseInt(i))
+                                                        {
+                                                            case 0:
+                                                                events[e].teams[team].label = '(C)'
+                                                                break
+                                                            case 1:
+                                                            case 2:
+                                                            case 3:
+                                                                events[e].teams[team].label = `(${i})`
+                                                                break
+                                                        }
+                                                    }
+                                                }
+                                                break
+                                        }
+                                    }
 
                                     if (++received == keys.length)
                                     {
+                                        console.log(events)
                                         // sort events by date then teams
-                                        keys.sort((a, b) => events[b].teams.length - events[a].teams.length)
+                                        keys.sort((a, b) => Object.keys(events[b].teams).length - Object.keys(events[a].teams).length)
                                         keys.sort((a, b) => new Date(events[a].start) - new Date(events[b].start))
 
                                         // add each event to the table
                                         for (let r of keys)
                                         {
                                             let event = events[r]
+                                            let keys = Object.keys(event.teams)
                                             if (!event_id.endsWith(r))
                                             {
                                                 // sort teams and add row
-                                                event.teams.sort((a, b) => parseInt(a) - parseInt(b))
-                                                document.getElementById('table').innerHTML += `<tr><td>${event.name}</td><td>${year}${r}</td><td>${event.start.replaceAll(`${year}-`, '')}</td><td>${event.teams.length}</td><td>${event.teams.join(', ')}</td></tr>`
+                                                keys.sort((a, b) => parseInt(a) - parseInt(b))
+                                                let teams = keys.map(t => `<span style="${event.teams[t].award}">${t}${event.teams[t].label}</span>`).join(', ')
+                                                document.getElementById('table').innerHTML += `<tr><td>${event.name}</td><td>${year}${r}</td><td>${event.start.replaceAll(`${year}-`, '')}</td><td>${keys.length}</td><td>${teams}</td></tr>`
                                             }
                                             else
                                             {
                                                 // add simple row for current event and update summary
-                                                document.getElementById('table').innerHTML += `<tr style="background-color: gray"><td>${event.name}</td><td>${event_id}</td><td>${event.start.replaceAll(`${year}-`, '')}</td><td>${event.teams.length}</td><td></td></tr>`
-                                                document.getElementById('summary').innerHTML = `Of the ${event.teams.length} teams attending the ${event.name}...`
+                                                document.getElementById('table').innerHTML += `<tr style="background-color: gray"><td>${event.name}</td><td>${event_id}</td><td>${event.start.replaceAll(`${year}-`, '')}</td><td>${keys.length}</td><td></td></tr>`
+                                                document.getElementById('summary').innerHTML = `Of the ${keys.length} teams attending the ${event.name}...`
                                             }
                                         }
                                     }
