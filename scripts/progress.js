4ef96b5cca13de7d588b562f08f6311c2561bfa2
Liam Fruzyna <liamfruzyna@gmail.com>
2022-05-12 16:36:58 -0500

    Updated scouting progress page

--- scripts/progress.js
+++ scripts/progress.js
@@ -5,78 +5,72 @@
  * date:        2022-01-31
  */
 
-// read parameters from URL
-const event_id = get_parameter(EVENT_COOKIE, EVENT_DEFAULT)
-
 /**
  * function:    init_page
  * parameters:  none
  * returns:     none
  * description: Runs onload to fill out the page.
  */
-function init_page()
-{
-    // set header
-    document.getElementById('header_info').innerHTML = 'Scouting Progress'
-    
-    let match_table = '<table><tr><td></td>'
-    let pit_table = '<table><tr>'
-    
-    // build match result table
-    let file_name = get_event_matches_name(event_id)
-    if (localStorage.getItem(file_name) != null)
-    {
-        let matches = JSON.parse(localStorage.getItem(file_name))
-        matches.sort((a, b) => a.time - b.time)
-        let keys = get_team_keys(event_id)
-        for (let key of keys)
-        {
-            match_table += `<th>${key}</th>`
-        }
-        match_table += '</tr>'
-        for (let match of matches)
-        {
-            match_table += `<tr><th>${match.match_number}</th>`
-            for (let key of keys)
-            {
-                let team = extract_match_teams(match)[key.toLowerCase().replaceAll(' ', '')]
-                let color = file_exists(get_match_result(match.match_number, team, event_id)) ? 'green' : 'red'
-                match_table += `<td style="background-color: ${color}">${team}</td>`
-            }
-            match_table += '</tr>'
-        }
-        match_table += '</table>'
-    }
-    
-    // build pit result table
-    file_name = get_event_teams_name(event_id)
-    if (localStorage.getItem(file_name) != null)
-    {
-        let teams = JSON.parse(localStorage.getItem(file_name))
-        teams.sort((a, b) => a.team_number - b.team_number)
-        for (let i in teams)
-        {
-            if (i % 7 == 0)
-            {
-                if (i != 0)
-                {
-                    pit_table += '</tr>'
-                }
-                pit_table += '<tr>'
-            }
-            let color = file_exists(get_pit_result(teams[i].team_number, event_id)) ? 'green' : 'red'
-            pit_table += `<td style="background-color: ${color}">${teams[i].team_number}</td>`
-        }
-        pit_table += '</tr></table>'
-    }
-
-    // add both tables to the page in cards
-    document.body.innerHTML += build_page_frame('', [
-        build_column_frame('Pit Progress', [
-            build_card('pits', pit_table)
-        ]),
-        build_column_frame('Match Progress', [
-            build_card('matches', match_table)
-        ])
-    ])
-}
+ function init_page()
+ {
+     // set header
+     document.getElementById('header_info').innerHTML = 'Scouting Progress'
+     
+     let match_table = '<table><tr><td></td>'
+     let pit_table = '<table><tr>'
+     
+     // build match result table
+     let matches = Object.keys(dal.matches)
+     matches.sort((a, b) => dal.get_match_value(a, 'scheduled_time') - dal.get_match_value(b, 'scheduled_time'))
+     let keys = Object.values(dal.get_team_keys(event_id))
+     for (let key of keys)
+     {
+         match_table += `<th>${key}</th>`
+     }
+     match_table += '</tr>'
+     for (let match of matches)
+     {
+         match_table += `<tr><th>${dal.get_match_value(match, 'short_match_name')}</th>`
+         let teams = Object.values(dal.get_match_teams(match))
+         for (let team of teams)
+         {
+             let color = dal.is_match_scouted(match, team) ? 'green' : 'red'
+             match_table += `<td style="background-color: ${color}">${team}</td>`
+         }
+         match_table += '</tr>'
+     }
+     match_table += '</table>'
+     
+     // build pit result table
+     let teams = Object.keys(dal.teams)
+     teams.sort((a, b) => a.team_number - b.team_number)
+     for (let i in teams)
+     {
+         if (i % 7 == 0)
+         {
+             if (i != 0)
+             {
+                 pit_table += '</tr>'
+             }
+             pit_table += '<tr>'
+         }
+         let color = dal.is_pit_scouted(teams[i]) ? 'green' : 'red'
+         pit_table += `<td style="background-color: ${color}">${teams[i]}</td>`
+     }
+     pit_table += '</tr></table>'
+ 
+     // add both tables to the page in cards
+     let page = new PageFrame('', '')
+ 
+     let pit = new ColumnFrame('pit_page', 'Pit Progress')
+     page.add_column(pit)
+     let pit_card = new Card('pits', pit_table)
+     pit.add_input(pit_card)
+ 
+     let match = new ColumnFrame('match_page', 'Match Progress')
+     page.add_column(match)
+     let match_card = new Card('matches', match_table)
+     match.add_input(match_card)
+ 
+     document.body.innerHTML += page.toString
+ }
