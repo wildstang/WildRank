278bb96341a9a53064c67542cd0b160e21b63c65
Liam Fruzyna <liamfruzyna@gmail.com>
2022-07-24 14:15:11 -0500

    Fixed WildRank not returning to index from home

--- scripts/blank.js
+++ scripts/blank.js
@@ -13,16 +13,23 @@ window.addEventListener('load', function()
 
 /**
  * function:    home()
- * parameters:  none
+ * parameters:  right click
  * returns:     none
  * description: Opens the appropriate home page, based on the current page.
  */
-function home()
+function home(right=false)
 {
     let url = 'index.html?page=home'
     if (page == 'home' || page == 'index' || page == '')
     {
         url = 'index.html' 
     }
-    window_open(url, '_self')
+    if (!right)
+    {
+        window_open(url, '_self')
+    }
+    else
+    {
+        window_open(url, '_blank')
+    }
 }
