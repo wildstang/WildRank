c65765197fcf39c9332c7a06d93a01de19089070
Liam Fruzyna <liamfruzyna@gmail.com>
2022-10-02 15:55:22 -0500

    Improvements to profile page test

--- scripts/misc/team-profile.js
+++ scripts/misc/team-profile.js
@@ -75,7 +75,7 @@ function process_team(team_nums)
                     robots[title] = {
                         code: `https://github.com/wildstang/${year}${modifier}_software`,
                         photos: [],
-                        awards: []
+                        awards: {}
                     }
                     
                     // fetch list of team-year's awards
@@ -89,9 +89,12 @@ function process_team(team_nums)
                         .then(awards => {
                             for (let award of awards)
                             {
-                                robots[title].awards.push({
+                                if (!robots[title].awards.hasOwnProperty(award.event_key))
+                                {
+                                    robots[title].awards[award.event_key] = []
+                                }
+                                robots[title].awards[award.event_key].push({
                                     type: award.award_type,
-                                    event: award.event_key,
                                     name: award.name
                                 })
                             }
@@ -157,11 +160,20 @@ function build_cards(robots)
         let image = `No pictures found. <a href="https://www.thebluealliance.com/suggest/team/media?team_key=frc${parts[1]}&year=${parts[0]}">Add one to TBA.</a>`
         if (robots[year].photos.length > 0)
         {
-            image = `<img src="${robots[year].photos[0]}" width="500px">`
+            image = `<img src="${robots[year].photos[0]}" height="400px">`
         }
         let code = `<a href="${robots[year].code}">Source Code</a>`
-        let awards = robots[year].awards.map(a => `${a.event}: ${a.name}`).join('<br>')
-        let card = new Card(year, `<h1>${year}</h1>${image}<br>${code}<br>${awards}`)
+        let tba = `<a href="https://www.thebluealliance.com/team/${parts[1]}/${parts[0]}">Season Stats</a>`
+        let awards = ''
+        let events = Object.keys(robots[year].awards)
+        for (let event of events)
+        {
+            let event_awards = robots[year].awards[event]
+            awards += `<br><b>${event}</b><br>`
+            awards += event_awards.map(a => a.name).join('<br>')
+        }
+        let card = new Card(year, `<h1>${year}</h1>${image}<div class="card_body"><div class="links">${code}${tba}</div><div class="awards">${awards}</div></div>`)
+        card.add_class('profile')
         if (left)
         {
             left_cards.push(card)
