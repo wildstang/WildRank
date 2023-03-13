844bece070cd941a4cd3ccb28d8a67a2a482ee46
Liam Fruzyna <liam@fruzyna.net>
2023-03-13 17:28:03 -0500

    Fixed original scouters not being able to edit match results

--- scripts/matches.js
+++ scripts/matches.js
@@ -163,5 +163,5 @@ function open_match(match_num)
  */
 function can_edit(match_num, team_num)
 {
-    return dal.get_result_value(team_num, match_num, 'meta_scouter_id') === user_id || cfg.is_admin(user_id)
+    return dal.get_result_value(team_num, match_num, 'meta_scouter_id') === parseInt(user_id) || cfg.is_admin(user_id)
 }
