883a15acdb4613daac9c0f1db3651e0bdadf9c2d
Liam Fruzyna <liam@fruzyna.net>
2023-12-22 17:19:22 -0600

    Fixed teams avatar and mini picklists

--- scripts/teams.js
+++ scripts/teams.js
@@ -25,10 +25,12 @@ function init_page()
     if (first)
     {
         avatar = document.createElement('img')
+        avatar.id = 'avatar'
 
         let team_header = document.createElement('h2')
         team_num_hdr = document.createElement('span')
         team_num_hdr.innerText = 'No Team Selected'
+        team_num_hdr.id = 'team_num'
         team_name = document.createElement('span')
         team_header.append(team_num_hdr, ' ', team_name)
 
