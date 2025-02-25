8e58fab434cb22018cd59d68ec20c03a842b5c05
Liam Fruzyna <liam@fruzyna.net>
2025-02-25 15:32:45 -0600

    WIP setup page to eventually replace index

---
+++ scripts/setup.js
@@ -0,0 +1,263 @@
+/**
+ * file:        setup.js
+ * description: Guides the user through app setup.
+ * author:      Liam Fruzyna
+ * date:        2025-02-25
+ */
+
+include('transfer')
+
+var event_id_el, user_id_el, position_el
+
+var user_id = ''
+var position = -1
+
+/**
+ * Initializes necessary variables and calls the first page setup.
+ */
+function init_page()
+{
+    user_id = get_cookie(USER_COOKIE, '')
+    position = get_cookie(POSITION_COOKIE, -1)
+
+    step_setup()
+}
+
+/**
+ * Builds the contents of the page. This is repeatedly called as progress is made.
+ */
+function step_setup()
+{
+    // use the event short name, if not available use the ID
+    let event = dal.event && dal.event.short_name !== undefined ? dal.event.short_name : event_id
+
+    // the primary column for performing setup
+    let setup_col = new WRColumn('Setup')
+    let columns = [setup_col]
+
+    // count the number of teams and matches for the event
+    let team_count = Object.keys(dal.teams).length
+    let match_count = Object.keys(dal.matches).length
+
+    // build a second column for viewing status info on the event and pulling in new configs
+    let status_col = new WRColumn('Status', [])
+    let event_config = new WRStatusTile(event)
+    event_config.set_status((team_count > 0 ? 1 : 0) + (match_count > 0 ? 1 : 0) - 1)
+    status_col.add_input(event_config)
+    let event_counts = new WRMultiNumber('', ['Team', 'Match'], [team_count, match_count])
+    status_col.add_input(event_counts)
+    scout_config_valid = new WRStatusTile(cfg.version)
+    scout_config_valid.set_status(cfg.validate_game_configs())
+    status_col.add_input(scout_config_valid)
+    status_col.add_input(new WRButton('Import Config', import_config))
+    if (TBA_KEY || cfg.keys.hasOwnProperty('tba'))
+    {
+        status_col.add_input(new WRButton('Load from TBA', preload_event))
+    }
+
+    // button used to trigger a fresh start of the setup
+    let reset = new WRButton('Restart Setup', restart_setup)
+    reset.add_class('slim')
+
+    // if an invalid event ID is provided prompt for it first
+    if (event_id.length < 8)
+    {
+        event_id_el = new WREntry('Event ID')
+        event_id_el.value = event_id
+        setup_col.add_input(event_id_el)
+
+        setup_col.add_input(new WRButton('Next', set_event_id))
+    }
+    // if an invalid user ID is provided prompt for it second, also add the event status column
+    else if (user_id.length !== 6)
+    {
+        user_id_el = new WREntry('School ID', 111112)
+        user_id_el.type = 'number'
+        user_id_el.bounds = [100000, 999999]
+        user_id_el.value = user_id
+        setup_col.add_input(user_id_el)
+
+        setup_col.add_input(new WRButton('Next', set_user_id))
+        setup_col.add_input(reset)
+
+        columns.push(status_col)
+    }
+    // the final page continues to show event status and prompts for position and scouting type
+    else
+    {
+        position_el = new WRDropdown(`${cfg.get_name(user_id)}'s Position`)
+        for (let i = 1; i <= dal.alliance_size * 2; i++)
+        {
+            let color = 'Red'
+            let pos = i
+            if (i > dal.alliance_size)
+            {
+                color = 'Blue'
+                pos = i - dal.alliance_size
+            }
+            position_el.add_option(`${color} ${pos}`)
+        }
+        position_el.value = position_el.options[0]
+        setup_col.add_input(position_el)
+
+        let match_scout = new WRButton('Match Scout', () => scout('matches'))
+        let other_scout = new WRMultiButton('', ['Pit', 'Alliance'], [() => scout('pits', () => scout('notes'))])
+        other_scout.add_class('slim')
+
+        let scout = new WRStack([match_scout, other_scout])
+        setup_col.add_input(scout)
+
+        let roles = new WRButton('Other Roles', () => window_open('index.html', '_self'))
+        roles.add_class('slim')
+        setup_col.add_input(roles)
+
+        setup_col.add_input(reset)
+
+        columns.push(status_col)
+    }
+
+    let page = new WRPage('')
+    for (let col of columns)
+    {
+        page.add_column(col)
+    }
+    body.replaceChildren(page)
+}
+
+/**
+ * Callback for when the event ID is set. Stores it if valid and reloads the config.
+ */
+function set_event_id()
+{
+    let id = event_id_el.element.value
+    if (id.length >= 8)
+    {
+        event_id = id
+        set_cookie(EVENT_COOKIE, event_id)
+
+        let year = event_id.substring(0, 4)
+        cfg = new Config(year)
+        cfg.load_configs()
+        dal = new DAL(event_id)
+        dal.build_teams()
+
+        step_setup()
+    }
+    else
+    {
+        alert('Invalid event ID')
+    }
+}
+
+/**
+ * Callback for when the import button is clicked. Starts the ZipHandler.
+ */
+function import_config()
+{
+    let handler = new ZipHandler()
+    handler.event       = true
+    handler.config      = true
+    handler.smart_stats = true
+    handler.coach       = true
+    handler.settings    = true
+    handler.pictures    = true
+    handler.always_overwrite = true
+    handler.on_complete = step_setup
+    handler.server      = parse_server_addr(document.location.href)
+    handler.import_zip_from_file(false, true)
+}
+
+/**
+ * Callback for when the restart button is clicked. Clears all inputs.
+ */
+function restart_setup()
+{
+    event_id = ''
+    user_id = ''
+    position = -1
+    step_setup()
+}
+
+/**
+ * Callback for when the user ID is set. Stores it if valid.
+ */
+function set_user_id()
+{
+    let id = user_id_el.element.value
+    if (id.length === 6)
+    {
+        user_id = id
+        set_cookie(USER_COOKIE, user_id)
+
+        let name = cfg.get_name(id)
+        if (cfg.is_admin(id))
+        {
+            name += ' (Admin)'
+        }
+        if (name !== cfg.get_name())
+        {
+            alert(`Welcome ${name}!`)
+        }
+
+        step_setup()
+    }
+    else
+    {
+        alert('Invalid user ID')
+    }
+}
+
+/**
+ * Callback for when the scout buttons are pressed. Reads in the position and goes to the appropriate selection page.
+ */
+function scout(mode)
+{
+    let team_count = Object.keys(dal.teams).length
+    let match_count = Object.keys(dal.matches).length
+
+    position = position_el.element.selectedIndex
+    if (position > 0 && ['matches', 'notes'].includes(mode))
+    {
+        set_cookie(POSITION_COOKIE, position)
+        if (team_count && match_count)
+        {
+            if (mode === 'matches')
+            {
+
+            }
+            else
+            {
+                alert('No matches available!')
+            }
+        }
+    }
+    else if (mode === 'pit')
+    {
+        if (team_count)
+        {
+        }
+        else
+        {
+            alert('No matches available!')
+        }
+    }
+}
+
+/**
+ * Used by external functions to get the current event ID.
+ */
+function get_event()
+{
+    return event_id
+}
+
+/**
+ * Used by externel functions when new data is acquired. Reloads the DAL.
+ */
+function process_files()
+{
+    dal = new DAL(event_id)
+    dal.build_teams()
+
+    step_setup()
+}
