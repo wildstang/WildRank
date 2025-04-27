e07295d74d193b69bab3af9588b4516df2a7ccfc
Liam Fruzyna <liam@fruzyna.net>
2025-04-27 12:05:19 -0500

    Updated config-debug

--- scripts/page/config-debug.js
+++ scripts/page/config-debug.js
@@ -6,46 +6,39 @@
  */
 
 /**
- * function:    init_page
- * parameters:  none
- * returns:     none
- * description: Runs onload to fill out the page.
+ * Populates the contents of the page.
  */
 function init_page()
 {
     // set header
     header_info.innerText = 'Config Debug'
 
-    let app = new WRStatusTile('App')
-    let user = new WRStatusTile('User')
-
-    let analysis = new WRStatusTile('Analysis')
-    let game = new WRStatusTile('Game')
-    let scout = new WRStatusTile('Scout')
-
-    validate(app, cfg.app)
-    validate(user, cfg.user)
-    validate(analysis, cfg.analysis)
-    validate(game, cfg.game)
-    validate(scout, cfg.scout)
-
-    let page = new WRPage('', [new WRColumn('Settings', [app, user])])
+    let page = new WRPage('', [new WRColumn('Settings', [validate('App'), validate('User')])])
     if (cfg.year)
     {
-        page.add_column(new WRColumn(`${cfg.year} Config`, [analysis, game, scout]))
+        page.add_column(new WRColumn(`${cfg.year} Config`, [validate('Analysis'),
+            validate('Game'), validate('Scout')]))
     }
 
     body.replaceChildren(page)
 }
 
 /**
- * Runs a validation of the given config and applies the result to the given status tile.
- * @param {WRStatusTile} tile Status tile to update
- * @param {Object} config Config object to validate
+ * Builds a status tile to represent the config file with the given name.
+ * @param {String} config_name Config file name
+ * @returns A WRStatusTile representing the config file
  */
-function validate(tile, config)
+function validate(config_name)
 {
+    let config = cfg[config_name.toLowerCase()]
+    let name = `${config_name} ${config.version}`
+    if (name.length > 17)
+    {
+        name = config.version
+    }
+    let tile = new WRStatusTile(name)
     let errors = config.validate(false).filter(t => t !== true)
     tile.set_status(errors.every(b => b === true) ? 1 : -1)
     tile.description = errors.join('\n\n')
+    return tile
 }
